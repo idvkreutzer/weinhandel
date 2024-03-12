bcefeb758892bb9a67b334d42afe2570830c613d
mkre <mkre@idv-ag,de>
2024-03-12 15:21:46 +0100

    grundgeruest

--- src/main/kotlin/de/idv/weinhandel/WeinhandelApplication.kt
+++ src/main/kotlin/de/idv/weinhandel/WeinhandelApplication.kt
@@ -1,22 +1,13 @@
 package de.idv.weinhandel
 
-import de.idv.weinhandel.repository.WeinRepository
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
-import org.springframework.context.annotation.Configuration
 
 @SpringBootApplication
 @ComponentScan
-@Configuration
 class WeinhandelApplication
 
-@Bean
-fun weinRepository() = WeinRepository()
-
 fun main(args: Array<String>) {
-    var context = runApplication<WeinhandelApplication>(*args)
-
-
+    runApplication<WeinhandelApplication>(*args)
 }
